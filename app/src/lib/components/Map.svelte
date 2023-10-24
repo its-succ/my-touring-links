db8308ba3a42f1f5a62dd03e9bcf1300792911f6
sizuhiko <k-kishida@esm.co.jp>
2023-10-24 16:11:48 +0900

    add Tabs for managing departure dates

--- app/src/lib/components/Map.svelte
+++ app/src/lib/components/Map.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
   import { PUBLIC_GMAP_API_KEY } from '$env/static/public';
   import { onMount } from "svelte";
+  import { DateTime } from 'luxon';
   import pWaitFor from 'p-wait-for';
   import Place from './Place.svelte';
+  import Tabs from './Tabs.svelte';
 
   let map: google.maps.MapElement;
   let placePicker: { value: google.maps.places.Place; };
-  let center: string | google.maps.LatLng = '35.684022,139.774474';
+  let center: string | google.maps.LatLng;
   let place: google.maps.places.Place | undefined;
   let placeId = 'ChIJp2YSkviJGGARyhnZ3I29Gzo';
+  let tabs = [new Date('2023-11-02T08:00+09:00')];
+  let active = tabs[0];
 
   onMount(async () => {
     await pWaitFor(() => map.innerMap !== undefined);
     map.innerMap.addListener('click', onCLickMap);
+    center = new google.maps.LatLng({ lat: 35.684022, lng: 139.774474});
   });
 
   function placechange() {
@@ -27,6 +32,20 @@
       placeId = iconMouseEvent.placeId!;
       center = iconMouseEvent.latLng!;
     }
+  }
+
+  function changeDepartureDateTime() {
+    console.log('change datetime');
+  }
+
+  function addTab() {
+    tabs = [...tabs, DateTime.fromJSDate(tabs[tabs.length - 1]).plus({ days: 1 }).toJSDate()];
+  }
+
+  function closeTab() {
+    const activeIndex = tabs.indexOf(active);
+    tabs = tabs.filter((p) => p !== active);
+    setTimeout(() => active = tabs[activeIndex] ?? tabs[activeIndex - 1]);
   }
 </script>
 
@@ -61,7 +80,9 @@
     </gmp-map>
   </div>
   <div slot="fixed">
-    <h3>More information</h3>
-    <Place placeId="{placeId}"></Place>
+    <Tabs planDates={tabs} bind:active={active} on:add={addTab} on:close={closeTab} on:changeDepartureDateTime={changeDepartureDateTime}></Tabs>
+    <div class="places">
+      <Place placeId="{placeId}"></Place>
+    </div>
   </div>
 </gmpx-split-layout>
---
+++ app/src/lib/components/Tabs.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import { DateTime } from 'luxon';
+  import Button from '@smui/button';
+  import Tab, { Label, Icon } from '@smui/tab';
+  import TabBar from '@smui/tab-bar';
+  import { createEventDispatcher } from 'svelte';
+
+  /** PlanDates */
+  export let planDates: Date[];
+  export let active: Date;
+
+  const dispatch = createEventDispatcher();
+
+  /**
+   * アクティブなタブの出発日時を変更する
+   */
+  function changeDepartureDateTime() {
+    dispatch('changeDepartureDateTime');
+  }
+  /**
+   * タブを追加する
+   */
+  function add() {
+    dispatch('add');
+  }
+  /**
+   * タブを閉じる
+   */
+   function close() {
+    dispatch('close');
+  }
+</script>
+
+<style>
+  :global(.tabicon) {
+    font-size:12px;
+    line-height:24px;
+    margin-right: -24px;
+  }
+  nav {
+    display: block;
+    text-align: right;
+    padding: 1em;
+  }
+</style>
+
+<TabBar tabs={planDates} let:tab bind:active>
+  <Tab {tab} minWidth>
+    <Label>{DateTime.fromJSDate(tab).setZone('Asia/Tokyo').toFormat('yyyy/MM/dd（EEEEE）HH:mm', { locale: 'ja' })}</Label>
+  </Tab>
+  {#if tab === planDates[planDates.length - 1]}
+    <Tab tab={'+'} minWidth on:click={add}>
+      <Icon class="material-icons">add</Icon>
+    </Tab>
+  {/if}
+</TabBar>
+<nav>
+  <Button color="secondary" on:click={changeDepartureDateTime}>
+    <Label>出発日時を変更する</Label>
+    <Icon class="material-icons navicon">settings</Icon>
+  </Button>
+  <Button color="secondary" disabled={planDates.length === 1} on:click={close}>
+    <Label>タブを閉じる</Label>
+    <Icon class="material-icons navicon">close</Icon>
+  </Button>
+</nav>
