acf54e15feb8a4668b07eec0436cb95b3615ce59
sizuhiko <k-kishida@esm.co.jp>
2023-11-10 17:58:10 +0900

    change DateTimePicker library to svelty-picker

--- app/src/lib/components/DateTimePicker.svelte
+++ app/src/lib/components/DateTimePicker.svelte
@@ -1,45 +1,59 @@
 <script lang="ts">
-  import '@siemens/ix/dist/siemens-ix/theme/classic-light.css';
-  import { applyPolyfills, defineCustomElements } from '@siemens/ix/loader';
-  import { defineCustomElements as ixIconsDefineCustomElements } from '@siemens/ix-icons/loader';
-  import type { IxDatetimePicker } from '@siemens/ix/components/ix-datetime-picker';
-  import Dialog, { Content } from '@smui/dialog';
-  import { DateTime } from 'luxon';
   import { createEventDispatcher } from 'svelte';
-
-  (async () => {
-    await applyPolyfills();
-    await ixIconsDefineCustomElements();
-    await defineCustomElements();
-  })();
+  import Dialog, { Content } from '@smui/dialog';
+  import SveltyPicker from 'svelty-picker';
 
   /** DateTimePicker の表示状態。true で表示する */
   let isOpen = false;
-  /** ix-datetime-picker タグ */
-  let picker: IxDatetimePicker;
-  let from: string;
-  let time: string;
+  /** 初期表示日時 */
+  let initialDate: Date;
+  /** DateTimePiker のローカライズ */
+  let jp = {
+    days:        ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日'],
+    daysShort:   ['日曜', '月曜', '火曜', '水曜', '木曜', '金曜', '土曜', '日曜'],
+    daysMin:     ['日', '月', '火', '水', '木', '金', '土', '日'],
+    months:      ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    monthsShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    meridiem:    ['am', 'pm'],
+    suffix:      ['', '', '', ''],
+    todayBtn:    '今日',
+    clearBtn:    'クリア',
+    okBtn:       'OK',
+    cancelBtn:   'Cancel',
+    timeView:    '時刻を選択',
+    backToDate:  'カレンダーに戻る'
+  }
 
-  const dispatch = createEventDispatcher();
-
+  /**
+   * DateTimePickerを開く
+   * @param date - 選択する日時
+   */
   export function open(date:Date) {
-    from = DateTime.fromJSDate(date).setZone('Asia/Tokyo').toFormat('yyyy-MM-dd');
-    time = DateTime.fromJSDate(date).setZone('Asia/Tokyo').toFormat('HH:mm');
+    initialDate = date;
     isOpen = true;
   }
 
-  function selected(e: CustomEvent<{ from?: string, time?: string}>) {
-    const date = new Date(`${e.detail!.from || picker.from}T${e.detail!.time || picker.time}+09:00`);
-    dispatch('selected', date);
+  /** イベントディスパッチャー */
+  const dispatch = createEventDispatcher();
+
+  /**
+   * OKボタンが押された時の処理。
+   * ダイアログを閉じて `selected` イベントを送信する
+   * @param e - OKボタンイベント
+   */
+  function selected(e: CustomEvent) {
+    isOpen = false;
+    dispatch('selected', new Date(e.detail));
+  }
+
+  /**
+   * Cancel ボタンが押された時の処理
+   * ダイアログを閉じる
+   */
+  function cancel() {
     isOpen = false;
   }
 </script>
-
-<style>
-  ix-datetime-picker {
-    --theme-z-index-dropdown: 1000;
-  }
-</style>
 
 <Dialog
   bind:open={isOpen}
@@ -49,18 +63,7 @@
 >
   <Content id="sheet-no-padding-content">
     {#if isOpen}
-      <ix-datetime-picker
-        class="theme-classic-light"
-        range="false"
-        show-hour="true"
-        show-minutes="true"
-        text-select-date="変更"
-        date-format="yyyy-MM-dd"
-        time-format="HH:mm"
-        from={from}
-        time={time}
-        bind:this={picker}
-        on:dateSelect={selected}></ix-datetime-picker>
+    <SveltyPicker autocommit={false} pickerOnly={true} mode="datetime" format="yyyy-mm-dd hh:ii" todayBtn={false} clearBtn={false}  i18n={jp} {initialDate} on:change={selected} on:cancel={cancel}  />
     {/if}
   </Content>
 </Dialog>
