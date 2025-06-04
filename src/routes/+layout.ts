6eb50bd0829cb14edd2b06144337085e497b02fe
sizuhiko <k-kishida@esm.co.jp>
2025-06-04 19:09:58 +0900

    remove firebase entries

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,25 +1,12 @@
 /** @type {import('./$types').LayoutLoad} */
 
-import { browser } from '$app/environment';
-import { auth, initializeFirebase } from '$lib/config/firebase';
-import { onAuthStateChanged, type User } from 'firebase/auth';
-
 export const prerender = true;
 
 export async function load({ url }) {
-  if (browser) {
-    try {
-      initializeFirebase();
-    } catch (ex) {
-      // eslint-disable-next-line no-console
-      console.error(ex);
-    }
-  }
-
-  function getAuthUser(): Promise<User | false> {
-    return new Promise((resolve) => {
-      onAuthStateChanged(auth, (user) => resolve(user ? user : false));
-    });
+  function getAuthUser(): Promise</** User */ object | false> {
+    // TODO: auth.js ベースでユーザー情報を返却するようにする
+    // 現時点は仮でログインしていないこととして false を戻す
+    return Promise.resolve(false)
   }
 
   return {
